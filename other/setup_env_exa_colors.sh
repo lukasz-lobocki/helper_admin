0b7e58a51f55e1fbdb8115bc744225aa3aa3e15a
lukasz-lobocki <lukasz.lobocki@googlemail.com>
2023-08-07 12:45:14 +0200

    fix: Fixing exa colors for lines.

--- other/setup_env_exa_colors.sh
+++ other/setup_env_exa_colors.sh
@@ -14,5 +14,5 @@ IFS=$'\n\t'
 grep --quiet 'EXA_COLORS=' ~/.bashrc || tee --append ~/.bashrc <<- 'EOF'
 
 # https://the.exa.website/
-export EXA_COLORS="da=38;5;12:gm=38;5;12:di=38;5;12;01"
+export EXA_COLORS="da=38;5;12:gm=38;5;12:di=38;5;12;01:xx=36"
 EOF
